40dccd7da3ea9234dfca4281b0dfd9c2612bb204
Daniel <dan_afram@hotmail.com>
2019-02-06 14:57:31 -0200

    classe Page criada

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php 
 
+//Sempre igual para criar as páginas
+//====================================
+//Traz as funções do composer, o que o projeto precisa
 require_once("vendor/autoload.php");
 
+//Namespaces, classes necessárias
+use \Slim\Slim;
+use \Hcode\Page;
+
+//Criando uma nova aplicação de Slim, uma nova rota, um caminho
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+//======================================
+
+//Executa função juntando o header, body e footer dos arquivos html, por fim o destrutor
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT*FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
+//Executa o site
 $app->run();
 
  ?>
